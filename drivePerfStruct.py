188a7b3e956475fdf2515e0f75c06b9d926ab8c4
bawells84 <benjamiw@netapp.com>
2013-12-09 16:43:01 -0600

    added new drivePerfStruct to drive performance data modified some code in volStruct to simply the data model added/updated regexes for drivePerfStruct

---
+++ drivePerfStruct.py
@@ -0,0 +1,387 @@
+import StringIO
+from regexes import *
+from types import DictType
+
+class ItnData(DictType):
+
+    global IOCOUNT, ERRCOUNT, REDUNDANCY, PERFORMANCE
+
+    IOCOUNT = int(0)
+    ERRCOUNT = int(1)
+    REDUNDANCY = int(2)
+    PERFORMANCE = int(3)
+
+    def __init__(self):
+
+        self[IOCOUNT] = {'r_success': '',
+                         'r_blks_xfer': '',
+                         'w_success': '',
+                         'w_blks_xfer': '',
+                         'queue_depth': '',
+                         'queued': '',
+                         'open_io': ''
+                         }
+
+        self[ERRCOUNT] = {'ch_errs': '',
+                          'hid_abts': '',
+                          'lid_det': '',
+                          'edc': '',
+                          'recovered': '',
+                          'not_ready': '',
+                          'medium': '',
+                          'hw': '',
+                          'ill_req': '',
+                          'unit_attn': '',
+                          'abt_cmd': '',
+                          'other': '',
+                          'busy': '',
+                          'resv_conf': '',
+                          'q_full': '',
+                          'aca_actv': '',
+                          'abort': ''
+                          }
+
+        self[REDUNDANCY] = {'o': '',
+                            'r': '',
+                            'p': '',
+                            'chn0_state': '',
+                            'chn1_state': '',
+                            'chn2_state': '',
+                            'chn3_state': ''
+                            }
+
+        self[PERFORMANCE] = {'r_art': '',
+                             'r_mrt': '',
+                             'w_art': '',
+                             'w_mrt': '',
+                             'old_cmd_age': '',
+                             'bsy_time': ''
+                             }
+
+
+class IterInstancesDrive(type):
+    def __iter__(cls):
+        return iter(cls._instances)
+
+
+class Drive():
+
+    __metaclass__ = IterInstancesDrive
+
+    _instances = []
+
+    def __init__(self):
+
+        self.devnum = ''
+        self.role = ''
+        self.pi = ''
+        self.capacity = ''
+        self.blksize = ''
+        self.itncnt = ''
+        self.onlineitncnt = ''
+        self.wwn = ''
+        self.ctrla = ItnData()
+        self.ctrlb = ItnData()
+
+        Drive._instances.append(self)
+
+    def new_with_drive(self, drive_data):
+        pass
+
+
+def get_ionshow12(statecapture):
+
+    statecapture.seek(0)
+    ionshow12 = StringIO.StringIO()
+    start_found = False
+
+    for line in statecapture:
+
+        start = find_ionshow12.search(line)
+        end = find_executing.search(line)
+
+        if start and not start_found:
+            start_found = True
+
+        elif start_found:
+            ionshow12.write(line)
+
+        elif end:
+            return ionshow12.getvalue()
+
+
+# Use passed in ionShow 12 output to generate drive objects
+
+def build_drives(buf):
+
+    for line in StringIO.StringIO(buf):
+
+        match = ionshow12_drive.search(line)
+
+        if match:
+
+            if match.group(5) == 'Open':
+
+                drv = Drive()
+                drv.devnum = match.group(1)
+                drv.role = match.group(2)
+                drv.capacity = match.group(6)
+                drv.pi = match.group(7)
+                drv.blksize = match.group(8)
+                drv.itncnt = match.group(9)
+                drv.onlineitncnt = match.group(10)
+                drv.wwn = match.group(11)
+
+            else:
+                continue
+        else:
+            continue
+
+
+def populate_drives_a(statecapture):
+
+    ctrl = 'a'
+    ionshow99 = StringIO.StringIO()
+    start_found = False
+
+    statecapture.seek(0)
+
+    for line in statecapture:
+
+        start = find_ionshow99_a.search(line)
+        end = find_executing.search(line)
+
+        if start and not start_found:
+            start_found = True
+
+        elif start_found:
+            ionshow99.write(line)
+
+        elif end:
+            break
+
+    process_luall0(ionshow99.getvalue(), ctrl)
+    process_luall2(ionshow99.getvalue(), ctrl)
+    process_luall3(ionshow99.getvalue(), ctrl)
+
+
+def populate_drives_b(statecapture):
+
+    ctrl = 'b'
+    ionshow99 = StringIO.StringIO()
+    start_found = False
+
+    statecapture.seek(0)
+
+    for line in statecapture:
+
+        start = find_ionshow99_b.search(line)
+        end = find_executing.search(line)
+
+        if start and not start_found:
+            start_found = True
+
+        elif start_found:
+            ionshow99.write(line)
+
+        elif end:
+            break
+
+    process_luall0(ionshow99.getvalue(), ctrl)
+    process_luall2(ionshow99.getvalue(), ctrl)
+    process_luall3(ionshow99.getvalue(), ctrl)
+
+# Takes ionShow99 output (buf) and a ctrl_slot ('a' or 'b') and populates a Drive object's relative stats
+# from the luall 0 output (ORP, Channel states, queue depth and queued commands, open commands, oldest command age)
+
+
+def process_luall0(buf, ctrl_slot):
+
+    luall0 = StringIO.StringIO()
+    start_found = False
+
+    for line in StringIO.StringIO(buf):
+
+        start = find_luall0.search(line)
+        end = nextionshowcommand.search(line)
+
+        if start and not start_found:
+            start_found = True
+
+        elif start_found:
+            luall0.write(line)
+
+        elif end:
+            break
+
+    for line in luall0:
+
+        match = luall0_drive.search(line)
+
+        if match:
+
+            for d in Drive._instances:
+
+                if d.devnum == match.group(1):
+
+                    if ctrl_slot == 'a':
+
+                        d.ctrla[IOCOUNT]['queue_depth'] = match.group(13)
+                        d.ctrla[IOCOUNT]['queued'] = match.group(14)
+                        d.ctrla[IOCOUNT]['open_io'] = match.group(15)
+                        d.ctrla[REDUNDANCY]['o'] = match.group(5)
+                        d.ctrla[REDUNDANCY]['r'] = match.group(6)
+                        d.ctrla[REDUNDANCY]['p'] = match.group(7)
+                        d.ctrla[REDUNDANCY]['chn0_state'] = match.group(8)
+                        d.ctrla[REDUNDANCY]['chn1_state'] = match.group(9)
+                        d.ctrla[REDUNDANCY]['chn2_state'] = match.group(10)
+                        d.ctrla[REDUNDANCY]['chn3_state'] = match.group(11)
+                        d.ctrla[PERFORMANCE]['old_cmd_age'] = match.group(18)
+
+                    elif ctrl_slot == 'b':
+
+                        d.ctrlb[IOCOUNT]['queue_depth'] = match.group(13)
+                        d.ctrlb[IOCOUNT]['queued'] = match.group(14)
+                        d.ctrlb[IOCOUNT]['open_io'] = match.group(15)
+                        d.ctrlb[REDUNDANCY]['o'] = match.group(5)
+                        d.ctrlb[REDUNDANCY]['r'] = match.group(6)
+                        d.ctrlb[REDUNDANCY]['p'] = match.group(7)
+                        d.ctrlb[REDUNDANCY]['chn0_state'] = match.group(8)
+                        d.ctrlb[REDUNDANCY]['chn1_state'] = match.group(9)
+                        d.ctrlb[REDUNDANCY]['chn2_state'] = match.group(10)
+                        d.ctrlb[REDUNDANCY]['chn3_state'] = match.group(11)
+                        d.ctrlb[PERFORMANCE]['old_cmd_age'] = match.group(18)
+                else:
+                    continue
+
+# Takes ionShow99 output (buf) and a ctlr_slot ('a' or 'b') and populates a Drive object's relative stats
+# from the luall 2 output (SCSI errors, SCSI statuses, HID and LID stats)
+
+
+def process_luall2(buf, ctrl_slot):
+
+    luall2 = StringIO.StringIO()
+    start_found = False
+
+    for line in StringIO.StringIO(buf):
+
+        start = find_luall2.search(line)
+        end = nextionshowcommand.search(line)
+
+        if start and not start_found:
+            start_found = True
+
+        elif start_found:
+            luall2.write(line)
+
+        elif end:
+            break
+
+    for line in luall2:
+
+        match = luall2_drive.search(line)
+
+        if match:
+
+            for d in Drive._instances:
+
+                if d.devnum == match.group(1):
+
+                    if ctrl_slot == 'a':
+
+                        d.ctrla[ERRCOUNT]['ch_errs'] = match.group(5)
+                        d.ctrla[ERRCOUNT]['hid_abts'] = match.group(6)
+                        d.ctrla[ERRCOUNT]['lid_det'] = match.group(7)
+                        d.ctrla[ERRCOUNT]['edc'] = match.group(8)
+                        d.ctrla[ERRCOUNT]['recovered'] = match.group(9)
+                        d.ctrla[ERRCOUNT]['not_ready'] = match.group(10)
+                        d.ctrla[ERRCOUNT]['medium'] = match.group(11)
+                        d.ctrla[ERRCOUNT]['hw'] = match.group(12)
+                        d.ctrla[ERRCOUNT]['ill_req'] = match.group(13)
+                        d.ctrla[ERRCOUNT]['unit_attn'] = match.group(14)
+                        d.ctrla[ERRCOUNT]['abt_cmd'] = match.group(15)
+                        d.ctrla[ERRCOUNT]['other'] = match.group(16)
+                        d.ctrla[ERRCOUNT]['busy'] = match.group(17)
+                        d.ctrla[ERRCOUNT]['resv_conf'] = match.group(18)
+                        d.ctrla[ERRCOUNT]['q_full'] = match.group(19)
+                        d.ctrla[ERRCOUNT]['aca_actv'] = match.group(20)
+                        d.ctrla[ERRCOUNT]['abort'] = match.group(21)
+
+                    elif ctrl_slot == 'b':
+
+                        d.ctrlb[ERRCOUNT]['ch_errs'] = match.group(5)
+                        d.ctrlb[ERRCOUNT]['hid_abts'] = match.group(6)
+                        d.ctrlb[ERRCOUNT]['lid_det'] = match.group(7)
+                        d.ctrlb[ERRCOUNT]['edc'] = match.group(8)
+                        d.ctrlb[ERRCOUNT]['recovered'] = match.group(9)
+                        d.ctrlb[ERRCOUNT]['not_ready'] = match.group(10)
+                        d.ctrlb[ERRCOUNT]['medium'] = match.group(11)
+                        d.ctrlb[ERRCOUNT]['hw'] = match.group(12)
+                        d.ctrlb[ERRCOUNT]['ill_req'] = match.group(13)
+                        d.ctrlb[ERRCOUNT]['unit_attn'] = match.group(14)
+                        d.ctrlb[ERRCOUNT]['abt_cmd'] = match.group(15)
+                        d.ctrlb[ERRCOUNT]['other'] = match.group(16)
+                        d.ctrlb[ERRCOUNT]['busy'] = match.group(17)
+                        d.ctrlb[ERRCOUNT]['resv_conf'] = match.group(18)
+                        d.ctrlb[ERRCOUNT]['q_full'] = match.group(19)
+                        d.ctrlb[ERRCOUNT]['aca_actv'] = match.group(20)
+                        d.ctrlb[ERRCOUNT]['abort'] = match.group(21)
+        else:
+            continue
+
+
+def process_luall3(buf, ctrl_slot):
+
+    luall3 = StringIO.StringIO()
+    start_found = False
+
+    for line in StringIO.StringIO(buf):
+
+        start = find_luall3.search(line)
+        end = nextionshowcommand.search(line)
+
+        if start and not start_found:
+            start_found = True
+
+        elif start_found:
+            luall3.write(line)
+
+        elif end:
+            break
+
+    for line in luall3:
+
+        match = luall3_drive.search(line)
+
+        if match:
+
+            for d in Drive._instances:
+
+                if d.devnum == match.group(1):
+
+                    if ctrl_slot == 'a':
+
+                        d.ctrla[IOCOUNT]['r_success'] = match.group(4)
+                        d.ctrla[IOCOUNT]['r_blks_xfer'] = match.group(5)
+                        d.ctrla[PERFORMANCE]['r_art'] = match.group(6)
+                        d.ctrla[PERFORMANCE]['r_mrt'] = match.group(7)
+                        d.ctrla[IOCOUNT]['w_success'] = match.group(8)
+                        d.ctrla[IOCOUNT]['w_blks_xfer'] = match.group(9)
+                        d.ctrla[PERFORMANCE]['w_art'] = match.group(10)
+                        d.ctrla[PERFORMANCE]['w_mrt'] = match.group(11)
+                        d.ctrla[PERFORMANCE]['bsy_time'] = match.group(14)
+
+                    elif ctrl_slot == 'b':
+
+                        d.ctrlb[IOCOUNT]['r_success'] = match.group(4)
+                        d.ctrlb[IOCOUNT]['r_blks_xfer'] = match.group(5)
+                        d.ctrlb[PERFORMANCE]['r_art'] = match.group(6)
+                        d.ctrlb[PERFORMANCE]['r_mrt'] = match.group(7)
+                        d.ctrlb[IOCOUNT]['w_success'] = match.group(8)
+                        d.ctrlb[IOCOUNT]['w_blks_xfer'] = match.group(9)
+                        d.ctrlb[PERFORMANCE]['w_art'] = match.group(10)
+                        d.ctrlb[PERFORMANCE]['w_mrt'] = match.group(11)
+                        d.ctrlb[PERFORMANCE]['bsy_time'] = match.group(14)
+
+            else:
+                continue
--- volStruct.py
+++ volStruct.py
@@ -24,8 +24,7 @@ class RAIDVolume(DictType):
     
     def __init__(self, buf):
 
-        self.vol = dict() 
-        self.vol[INFO] = {
+        self[INFO] = {
                             'ssid': "",
                             'user_label': "",
                             'capacity': "",
@@ -37,7 +36,7 @@ class RAIDVolume(DictType):
                             'preferred_owner': ""
         }
         
-        self.vol[CACHE] = {
+        self[CACHE] = {
                             'read_ahead': "",
                             'fast_write': "",
                             'flush_mod': "",
@@ -45,7 +44,7 @@ class RAIDVolume(DictType):
                             'cache_block': "",
         }
         
-        self.vol[IOSTAT] = {
+        self[IOSTAT] = {
                             'requests': "",
                             'blocks': "",
                             'avg_blocks': "",
@@ -54,7 +53,7 @@ class RAIDVolume(DictType):
                             'write_algorithm': ""
         }
         
-        self.vol[VGINFO] = {
+        self[VGINFO] = {
                             'vg_label': "",
                             'drive_count': "",
                             'boundary': "",
@@ -85,15 +84,15 @@ class RAIDVolume(DictType):
         owner = vol_ownership.search(buf)
         pref_own = vol_pref_ownership.search(buf)
         
-        self.vol[INFO]['ssid'] = ssid
-        self.vol[INFO]['user_label'] = user_label.group(1)
-        self.vol[INFO]['capacity'] = capacity.group(1)
-        self.vol[INFO]['blocksize'] = blksize.group(1)
-        self.vol[INFO]['segment_size'] = seg_size.group(1)
-        self.vol[INFO]['stripe_size'] = stripe_sz.group(1)
-        self.vol[INFO]['pre_read'] = pre_rd.group(1)
-        self.vol[INFO]['owner'] = owner.group(1)
-        self.vol[INFO]['preferred_owner'] = pref_own.group(1)
+        self[INFO]['ssid'] = ssid
+        self[INFO]['user_label'] = user_label.group(1)
+        self[INFO]['capacity'] = capacity.group(1)
+        self[INFO]['blocksize'] = blksize.group(1)
+        self[INFO]['segment_size'] = seg_size.group(1)
+        self[INFO]['stripe_size'] = stripe_sz.group(1)
+        self[INFO]['pre_read'] = pre_rd.group(1)
+        self[INFO]['owner'] = owner.group(1)
+        self[INFO]['preferred_owner'] = pref_own.group(1)
         
         rd_ahead = vol_cache_read_ahead.search(buf)
         fast_wr = vol_cache_fast_write.search(buf)
@@ -101,11 +100,11 @@ class RAIDVolume(DictType):
         warn_flush = vol_cache_min_warn_flush.search(buf)
         cache_blk = vol_cache_granularity.search(buf)
         
-        self.vol[CACHE]['read_ahead'] = rd_ahead.group(1)
-        self.vol[CACHE]['fast_write'] = fast_wr.group(1)
-        self.vol[CACHE]['flush_mod'] = flush_mod.group(1)
-        self.vol[CACHE]['warn_mod'] = warn_flush.group(1)
-        self.vol[CACHE]['cache_block'] = cache_blk.group(1)
+        self[CACHE]['read_ahead'] = rd_ahead.group(1)
+        self[CACHE]['fast_write'] = fast_wr.group(1)
+        self[CACHE]['flush_mod'] = flush_mod.group(1)
+        self[CACHE]['warn_mod'] = warn_flush.group(1)
+        self[CACHE]['cache_block'] = cache_blk.group(1)
         
         requests = vol_io_requests.search(buf)
         blocks = vol_io_blocks.search(buf)
@@ -114,7 +113,7 @@ class RAIDVolume(DictType):
         writes = vol_io_writes.search(buf)
         write_alg = vol_io_write_alg.search(buf)
         
-        self.vol[IOSTAT]['requests'] = {
+        self[IOSTAT]['requests'] = {
                                         'sm_reads': requests.group(1),
                                         'sm_writes': requests.group(2),
                                         'sm_writesame': requests.group(3),
@@ -125,11 +124,10 @@ class RAIDVolume(DictType):
                                         'total': requests.group(8),
                                         'cache_hits': requests.group(9)}
         
-        self.vol[IOSTAT]['blocks'] = {
+        self[IOSTAT]['blocks'] = {
                                         'sm_reads': blocks.group(1),
                                         'sm_writes': blocks.group(2),
-                                        'sm_writesame': blocks.group(3)
-                                        ,
+                                        'sm_writesame': blocks.group(3),
                                         'lg_reads': blocks.group(4), 
                                         'lg_writes': blocks.group(5),
                                         'lg_writesame': blocks.group(6),
@@ -137,7 +135,7 @@ class RAIDVolume(DictType):
                                         'total': blocks.group(8),
                                         'cache_hits': blocks.group(9)}
         
-        self.vol[IOSTAT]['avg_blocks'] = {
+        self[IOSTAT]['avg_blocks'] = {
                                         'sm_reads': avg_blocks.group(1),
                                         'sm_writes': avg_blocks.group(2),
                                         'sm_writesame': avg_blocks.group(3),
@@ -148,17 +146,17 @@ class RAIDVolume(DictType):
                                         'total': avg_blocks.group(8),
                                         'cache_hits': avg_blocks.group(9)}
         
-        self.vol[IOSTAT]['reads'] = {
+        self[IOSTAT]['reads'] = {
                                         'io': reads.group(1),
                                         'stripes': reads.group(2),
                                         'clusters': reads.group(3)}
         
-        self.vol[IOSTAT]['writes'] = {
+        self[IOSTAT]['writes'] = {
                                         'io': writes.group(1),
                                         'stripes': writes.group(2),
                                         'clusters': writes.group(3)}
         
-        self.vol[IOSTAT]['write_algorithm'] = {
+        self[IOSTAT]['write_algorithm'] = {
                                         'full': write_alg.group(1),
                                         'partial': write_alg.group(2),
                                         'RMW': write_alg.group(3),
@@ -171,10 +169,10 @@ class RAIDVolume(DictType):
         boundary = vol_vg_boundary.search(buf)
         media_type = vol_vg_media_type.search(buf)
         
-        self.vol[VGINFO]['vg_label'] = vg_label.group(1)
-        self.vol[VGINFO]['drive_count'] = drive_cnt.group(1)
-        self.vol[VGINFO]['boundary'] = boundary.group(1)
-        self.vol[VGINFO]['media_type'] = media_type.group(1)
+        self[VGINFO]['vg_label'] = vg_label.group(1)
+        self[VGINFO]['drive_count'] = drive_cnt.group(1)
+        self[VGINFO]['boundary'] = boundary.group(1)
+        self[VGINFO]['media_type'] = media_type.group(1)
 
     # Return a str of the RAIDVolume instance's SSID. dec is a bool, if True
     # value will be in base 10, if False it will be the stored SSID
@@ -183,10 +181,10 @@ class RAIDVolume(DictType):
     def get_ssid(self, dec):
         # Dec is a 'bool' to return a base 10 version of the SSID
         if dec:
-            s = str(int(self.vol[INFO]['ssid'], 16))
+            s = str(int(self[INFO]['ssid'], 16))
             return s
         else:
-            return self.vol[INFO]['ssid']
+            return self[INFO]['ssid']
 
     # Print the RAIDVolume instance's INFO
 
@@ -196,32 +194,32 @@ class RAIDVolume(DictType):
         ssid_dec = self.get_ssid(True)
         
         print "SSID       : " + ssid_dec + " (" + ssid_hex + ")"
-        print "User Label : " + self.vol[INFO]['user_label']
-        print "Capacity   : " + self.vol[INFO]['capacity']
-        print "BlockSize  : " + self.vol[INFO]['blocksize']
-        print "Stripe Size: " + self.vol[INFO]['stripe_size']
-        print "Pre-Read   : " + self.vol[INFO]['pre_read']
-        print "Cur. Owner : " + self.vol[INFO]['owner']
-        print "Pref Owner : " + self.vol[INFO]['preferred_owner']
+        print "User Label : " + self[INFO]['user_label']
+        print "Capacity   : " + self[INFO]['capacity']
+        print "BlockSize  : " + self[INFO]['blocksize']
+        print "Stripe Size: " + self[INFO]['stripe_size']
+        print "Pre-Read   : " + self[INFO]['pre_read']
+        print "Cur. Owner : " + self[INFO]['owner']
+        print "Pref Owner : " + self[INFO]['preferred_owner']
 
     # Print the RAIDVolume instance's CACHE
 
     def print_vol_cache(self):
         
-        print "Read Ahead      : " + self.vol[CACHE]['read_ahead']
-        print "Fast Write      : " + self.vol[CACHE]['fast_write']
-        print "Cache Flush Mod.: " + self.vol[CACHE]['flush_mod']
-        print "Min. Warn Mod.  : " + self.vol[CACHE]['warn_mod']
-        k = int(self.vol[CACHE]['cache_block']) * 512 / 1024
+        print "Read Ahead      : " + self[CACHE]['read_ahead']
+        print "Fast Write      : " + self[CACHE]['fast_write']
+        print "Cache Flush Mod.: " + self[CACHE]['flush_mod']
+        print "Min. Warn Mod.  : " + self[CACHE]['warn_mod']
+        k = int(self[CACHE]['cache_block']) * 512 / 1024
         print "Cache Block Size: " + str(k) + "K"
 
     # Print the RAIDVolume instance's VGINFO
     def print_vol_vginfo(self):
         
-        print "VG Label    : " + self.vol[VGINFO]['vg_label']
-        print "Drive Count : " + self.vol[VGINFO]['drive_count']
-        print "Boundary    : " + self.vol[VGINFO]['boundary']
-        print "Media Type  : " + self.vol[VGINFO]['media_type']
+        print "VG Label    : " + self[VGINFO]['vg_label']
+        print "Drive Count : " + self[VGINFO]['drive_count']
+        print "Boundary    : " + self[VGINFO]['boundary']
+        print "Media Type  : " + self[VGINFO]['media_type']
         
     def print_vol_iostats(self):
         pass
@@ -231,14 +229,14 @@ class RAIDVolume(DictType):
 
     def get_value_with_keys(self, cat, key):
         
-        return self.vol[cat][key]
+        return self[cat][key]
 
     # Direct access to a RAIDVolume instrance's IOSTAT dict
     # by passing an IOSTAT category and relative key.
 
     def get_iostats_with_keys(self, cat, key):
         
-        return self.vol[IOSTAT][cat][key]
+        return self[IOSTAT][cat][key]
     
 def build_raid_volumes(statecapture):
     # Generate all RAIDVolume objects with a passed in buffer
